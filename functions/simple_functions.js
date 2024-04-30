3d6f2396906f9f26a2c5b571d27d27aa1734b2f0
Antonio <asoriano@appdev.yearup.org>
2024-04-30 13:04:49 -0500

    completed simple_functions.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,21 @@
+"use strict"
+
+// function to display my name, favorite movie, favorite band
+function favorite_things() {
+    console.log("Antonio");
+    console.log("Whiplash");
+    console.log("Weezer");
+}
+
+function whyImHere() {
+    console.log("I am here to implement solutions with measurable, real-world applications");
+}
+
+function favoritePlace() {
+    console.log("My favorite place to visit is Kyoto because I have a deep appreciation for the architecture and culture");
+}
+
+// calling our functions
+favorite_things();
+whyImHere();
+favoritePlace();
