b9aa3ad944f5cd0446cf1a849942138d9dde724d
Antonio <asoriano@appdev.yearup.org>
2024-05-02 09:43:23 -0500

    completed parsing.js

---
+++ strings/parsing.js
@@ -0,0 +1,26 @@
+"use strict"
+
+function parseAndDisplay(someName) {
+
+    // create a variable to hold the first name
+    let fullName = someName;
+
+    // find the position of the first space
+    let spacePosition = fullName.indexOf(" ");
+
+    // create variable for the first and last name
+    let firstName = fullName.substring(0, spacePosition);
+    let lastName = fullName.substring(spacePosition + 1);
+
+
+    console.log(`
+    Name: ${fullName}
+    First name: ${firstName}
+    Last name: ${lastName}`
+    );
+}
+
+parseAndDisplay("Brenda Kaye");
+parseAndDisplay("Ian Auston");
+parseAndDisplay("Siddalee Grace");
+
